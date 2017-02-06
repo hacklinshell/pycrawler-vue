ff2c463ef0836d3edf3795f09d3023f83813a936
hacklinshell <hacklinshell@gmail.com>
2017-02-06 10:57:57 +0800

    前端vue结构修改以及登录页面完成

---
+++ src/components/Item.vue
@@ -0,0 +1,21 @@
+<template>
+    <button v-on:click="increment">{{ counter }}</button>
+</template>
+
+
+<script>
+    export default {
+        name: 'Item',
+        data: function() {
+            return {
+                counter: 0
+            }
+        },
+        methods: {
+            increment: function() {
+                this.counter += 1
+                this.$emit('increments')
+            }
+        },
+    }
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-row class="content">
+    <el-col :xs="24" :sm="{span: 6,offset: 9}">
+      <span class="title">欢迎登录</span>
+            <span class="title">福</span>
+
+      <el-row>
+        <el-input v-model="account" placeholder="账号" type="text"> </el-input>
+        <el-input v-model="password" placeholder="密码" type="password"></el-input>
+        <el-col :span="12">
+             <el-button type="primary" :click="btnSubmitForm('register')">注册</el-button>
+        </el-col>
+        <el-col :span="12">
+            <el-button type="primary" :click="btnSubmitForm('login')" >登录</el-button>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data() {
+            return {
+                account: '',
+                password: ''
+            }
+        },
+        methods: {
+            btnSubmitForm: function(form) {
+                if (form == 'login') {
+
+                } else if (form == "register") {
+
+                } else if (form == "forget-pwd") {
+
+                }
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style type="text/css" scoped>
+    .el-row content {
+        padding: 16px
+    }
+    
+    .title {
+        font-family: "Helvetica";
+        font-size: 28px
+    }
+    
+    .el-input {
+        margin: 12px 0
+    }
+    
+    .el-button {
+        width: 95%;
+    }
+</style>
